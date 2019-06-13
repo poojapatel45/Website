87228702608df3aca48d56b70d4e65a55557c749
poojapatel45 <32950431+poojapatel45@users.noreply.github.com>
2019-06-13 11:09:42 -0700

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,11 +1,7 @@
-// This is the "Offline page" service worker
-
 const CACHE = "pwabuilder-page";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
-// Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   console.log("[PWA Builder] Install Event processing");
 
@@ -22,7 +18,6 @@ self.addEventListener("install", function (event) {
   );
 });
 
-// If any fetch fails, it will show the offline page.
 self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") return;
 
@@ -44,7 +39,6 @@ self.addEventListener("fetch", function (event) {
   );
 });
 
-// This is an event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener("refreshOffline", function () {
   const offlinePageRequest = new Request(offlineFallbackPage);
 
